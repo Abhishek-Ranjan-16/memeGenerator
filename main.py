716a457b96ca2389a8e6bdbc3ab32fee0dd79bf1
AssassinOFsilchar <abhiran14082002@gmail.com>
2024-05-30 01:30:29 +0530

    made form, added multiple pictures

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,redirect,request
+import os
 import requests
 import json
+import random
 
 app=Flask(__name__)
 
 def get_meme(channel):
     sr=channel
-    url="https://meme-api.com/gimme/"+sr
-    # url="https://meme-api.com/gimme"
-    # jab bhi kuch api fetch krna toh iss tarah seh fetch krlena
-    response=json.loads(requests.request("GET",url).text) # yaha meh conversion chal rha hai json ka shayad
-
-    # yeh niche wala structure ke hisab seh select hua hai apne aap
+    url="https://meme-api.com/gimme/"+sr 
+    response=json.loads(requests.request("GET",url).text)  
     meme_large=response["preview"][-1]
     subreddit=response["subreddit"]
     return meme_large, subreddit
 
 @app.route("/")
-def callMeX(): 
-    return render_template("indra.html")
+def callMeX():  
+    img_dir = os.path.join(app.static_folder, 'images') 
+    images = [f for f in os.listdir(img_dir) if os.path.isfile(os.path.join(img_dir, f))]  
+    descriptions_path = os.path.join(os.path.dirname(__file__), 'images.json')
+    with open(descriptions_path, 'r') as f:
+        descriptions = json.load(f)
+    selected_image = random.choice(images)
+    description = descriptions.get(selected_image, "Lund mera") 
+
+    image_data = {
+        'url': url_for('static', filename=f'images/{selected_image}'),
+        'description': description
+    }  
+    return render_template('indra.html', image_data=image_data)
+ 
+@app.route('/navigate', methods=['POST'])
+def navigate(): 
+    variable_name = request.form['variable_name']  
+    return redirect(f'/{variable_name}')
+
+
 @app.route("/<channel>")
 def index(channel):
     meme_pic,subreddit=get_meme(channel)
